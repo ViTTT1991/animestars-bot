94ab421cfb51e504313d27dc2b01e0bff4fd6722
ViTTT1991 <vitt1991@gmail.com>
2025-03-31 02:33:48 +0300

    Fix playwright installation on Render

--- build.sh
+++ build.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env bash
 set -e
 
+# Установка системных зависимостей для Chromium
+apt-get update
+apt-get install -y \
+    libnss3 \
+    libatk1.0-0 \
+    libatk-bridge2.0-0 \
+    libcups2 \
+    libdrm2 \
+    libxkbcommon0 \
+    libxcomposite1 \
+    libxdamage1 \
+    libxfixes3 \
+    libxrandr2 \
+    libgbm1 \
+    libasound2 \
+    libpangocairo-1.0-0 \
+    libpango-1.0-0
+
 # Установка зависимостей Python
 pip install -r requirements.txt
 
